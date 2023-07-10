03ffa18eebd0dc0979d17f6a34ddec0edbe9de45
xiaoyang.yx <xiaoyang.yx@xuelangyun.com>
2023-07-10 11:41:02 +0800

    🔧 build(xuelang build):

---
+++ build.xuelang.sh
@@ -0,0 +1,2 @@
+docker build -t registry.xuelangyun.com/shuzhi-amd64/cv/label-studio:xuelang-latest -f ./Dockerfile.xuelang .
+docker push registry.xuelangyun.com/shuzhi-amd64/cv/label-studio:xuelang-latest
